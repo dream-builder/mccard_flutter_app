0838e6bc1b580d26eb55ac4ff7a292b885da7c4e
Shahed Chaklader <mohammad.shahed.chaklader@gmail.com>
2023-12-03 10:38:36 +0600

    MC Card Flutter App able to view mccard website

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyWebView(),
+    );
+  }
+}
+
+class MyWebView extends StatefulWidget {
+  @override
+  _MyWebViewState createState() => _MyWebViewState();
+}
+
+class _MyWebViewState extends State<MyWebView> {
+  @override
+  void initState() {
+    super.initState();
+    // Enable WebView JavaScript
+    WebView.platform = SurfaceAndroidWebView();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('WebView with JavaScript'),
+      ),
+      body: WebView(
+        initialUrl: 'http://google.com', // Replace with your desired URL
+        javascriptMode: JavascriptMode.unrestricted,
+      ),
+    );
+  }
+}
